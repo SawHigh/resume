8091c3acb9bfc1e013e7aa35a71b23074e33d4ac
kururu <stupidfool9413@gmail.com>
2016-02-05 12:31:27 +0800

    reverse_lazy

--- sawhighA/views.py
+++ sawhighA/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
 
 def home(request):
     return render(request, "home.html")
 
-@login_required(login_url=reverse("login"))
+@login_required(login_url=reverse_lazy("login"))
 def backend_home(request):
     return render(request, "backend_home.html")
