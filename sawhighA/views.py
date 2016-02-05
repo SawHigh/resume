73ddda9b44205522551f26640609151cd8cc9f69
kururu <stupidfool9413@gmail.com>
2016-02-05 12:27:24 +0800

    decrator

--- sawhighA/views.py
+++ sawhighA/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
 def home(request):
     return render(request, "home.html")
 
-login_required
+@login_required(login_url=reverse("login"))
 def backend_home(request):
     return render(request, "backend_home.html")
