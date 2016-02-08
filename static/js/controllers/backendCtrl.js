d57ff0062fbd0f332c94a4cd8930aa237e8c201f
Kihara <ryoukayuu@gmail.com>
2016-02-08 14:11:27 +0800

    datepicker

--- static/js/controllers/backendCtrl.js
+++ static/js/controllers/backendCtrl.js
@@ -1,6 +1,7 @@
 app.controller('profileCtrl', ['$scope', 'profile',function($scope,profile) {
     profile.success(function(data) {
-    $scope.profiles = data.data; 
+    $scope.profiles = data.data[0]; 
     console.log($scope.profiles);
     }); 
+    $('#birth input').datepicker({});
 }]);
