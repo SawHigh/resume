c1dee6cbc8d3e2c782e113517aca7adf149fe082
Kihara <ryoukayuu@gmail.com>
2016-02-10 15:56:16 +0800

    profile

--- static/js/controllers/backendCtrl.js
+++ static/js/controllers/backendCtrl.js
@@ -1,7 +1,13 @@
-app.controller('profileCtrl', ['$scope', 'profile',function($scope,profile) {
+app.controller('profileCtrl', ['$scope','$routeParams', 'profile',function($scope,$routeParams,profile) {
     profile.success(function(data) {
     $scope.profiles = data.data[0]; 
     console.log($scope.profiles);
     }); 
-    $('#birth input').datepicker({});
+    
+    if($routeParams.edit == "edit"){
+        $scope.readonly = false;
+        $('#birth input').datepicker({});
+    }else{
+        $scope.readonly = true;
+    };
 }]);
---
+++ static/js/directives/directives.js
@@ -0,0 +1,7 @@
+app.directive('profile',function($routeParams) {
+  return {
+    restrict: 'E',
+    scope: {},
+    templateUrl: '/static/js/directives/profile.html'
+  };
+});
