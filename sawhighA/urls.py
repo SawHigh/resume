53c59332dd5b68a2d4b2181001e187fb88d6e584
kururu <stupidfool9413@gmail.com>
2016-02-14 20:18:36 +0800

    测试用 media url

--- sawhighA/urls.py
+++ sawhighA/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^sawhigh/', include('resume.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
